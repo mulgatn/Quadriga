04d8d3f075ea5ee034e772847bbd0dce5ecef0b9
Peace <kbarisbicak@hotmail.com>
2019-04-18 16:52:32 +0200

    Input and Coliison

--- Quadriga/Assets/Scripts/Car_Controller.cs
+++ Quadriga/Assets/Scripts/Car_Controller.cs
@@ -15,10 +15,14 @@ public class Car_Controller : MonoBehaviour
     protected Rigidbody2D body;
     protected KeyCode turnLeft;
     protected KeyCode turnRight;
+    protected KeyCode goLeft;
+    protected KeyCode goRight;
     protected int playerNumber;
     private int lapCount;
     protected bool isWon;
     private bool isActive;
+    private bool goingLeft;
+    private bool goingRight;
 
     protected void Awake()
     {
@@ -27,6 +31,7 @@ public class Car_Controller : MonoBehaviour
         lapCount = 0;
         isWon = false;
         isActive = true;
+        speed += acceleration;
     }
 
     protected void Update()
@@ -37,6 +42,18 @@ public class Car_Controller : MonoBehaviour
                 rotate = Input.GetAxis("Player1_Rotation");
             else if (playerNumber == 2)
                 rotate = Input.GetAxis("Player2_Rotation");
+            if(rotate != 0f)
+                if (speed == minSpeed)
+                    speed += acceleration;
+            if (Input.GetKey(goLeft))
+                goingLeft = true;
+            else
+                goingLeft = false;
+            if (Input.GetKey(goRight))
+                goingRight = true;
+            else
+                goingRight = false;
+                
             if (Input.GetKey(turnLeft) && Input.GetKey(turnRight))
             {
                 if (rotate > 0)
@@ -46,16 +63,19 @@ public class Car_Controller : MonoBehaviour
                 speed -= breakPower;
                 breaking = true;
                 if (speed < minSpeed)
-                    speed = minSpeed;
+                    speed = minSpeed;     
             }
             else
             {
-                if (speed < maxSpeed)
+                if (speed < maxSpeed && speed != minSpeed)
                     speed += acceleration;
-                else speed = maxSpeed;
+                else if(speed > maxSpeed)
+                    speed = maxSpeed;
                 breaking = false;
             }
-            if (lapCount == 2)
+
+
+            if (lapCount == 4)
             {
                 PlayerPrefs.SetInt("Winner", playerNumber);
                 isWon = true;
@@ -65,17 +85,39 @@ public class Car_Controller : MonoBehaviour
     protected void FixedUpdate()
     {   
         if(isActive)
-        {
-            body.velocity = transform.up * speed;
+        {          
             if (!breaking)
                 body.angularVelocity = (rotate * torquePower);
+            if (body.velocity.magnitude < maxSpeed && speed != minSpeed)
+                body.AddForce(transform.up * acceleration * 10f);
+
+            if (goingRight || goingLeft)
+            {
+                if (goingRight)
+                {
+                    Vector2 temp = transform.right * 0.2f;
+                    body.velocity = body.velocity + temp;
+                }
+                else
+                {
+                    Vector2 temp = -transform.right * 0.2f;
+                    body.velocity = body.velocity + temp;
+                }
+            }
+            else
+                body.velocity = ForwardVelocity();
         }  
     }
-
+ 
     protected void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Start_Line")
             lapCount++;
+    }
+    protected void OnCollisionEnter2D(Collision2D other)
+    {
+        speed = minSpeed;
+        body.velocity = Vector2.zero;
     }
 
     public bool checkWin()
@@ -91,5 +133,10 @@ public class Car_Controller : MonoBehaviour
     public bool getActivity()
     {
         return isActive;
+    }
+
+    private Vector2 ForwardVelocity()
+    {
+        return transform.up * Vector2.Dot(body.velocity, transform.up);
     }
 }
