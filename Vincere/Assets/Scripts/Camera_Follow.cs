eaddcf3dd4ba348461782f925f3837a197f1126e
Peace <kbarisbicak@hotmail.com>
2019-04-25 22:53:29 +0200

    Camera Rotation Fixed

---
+++ Vincere/Assets/Scripts/Camera_Follow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour
+{
+    public Transform player;
+    public float rotationSpeed;
+
+    private void Update()
+    {
+        transform.rotation = Quaternion.Lerp(transform.rotation, player.rotation, rotationSpeed);
+    }
+}
